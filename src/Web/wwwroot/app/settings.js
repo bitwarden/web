4c0bde9d871f888eb2fbe75181b48c07b6d3f290
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-10-15 12:30:20 -0400

    1.2.0 version bump

--- src/Web/wwwroot/app/settings.js
+++ src/Web/wwwroot/app/settings.js
@@ -1,2 +1,2 @@
 angular.module("bit")
-.constant("appSettings", {"rememberedEmailCookieName":"bit.rememberedEmail","version":"1.1.1","environment":"Development","apiUri":"http://localhost:4000"});
+.constant("appSettings", {"rememberedEmailCookieName":"bit.rememberedEmail","version":"1.2.0","environment":"Development","apiUri":"http://localhost:4000"});
