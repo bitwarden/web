bca7592c7778ae569e7a141af872d57c97f17e40
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-02-01 22:56:39 -0500

    production by default settings

--- src/app/settings.js
+++ src/app/settings.js
@@ -1,2 +1,2 @@
 angular.module("bit")
-.constant("appSettings", {"rememberedEmailCookieName":"bit.rememberedEmail","apiUri":"http://localhost:4000","version":"1.8.1","environment":"Development"});
+.constant("appSettings", {"rememberedEmailCookieName":"bit.rememberedEmail","apiUri":"https://api.bitwarden.com","version":"1.8.1","environment":"Production"});
