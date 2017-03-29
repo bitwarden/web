af2f7a7a5a6c9426cff2aa89a9e958d59df64add
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-03-29 19:21:06 -0400

    organization listing from side menu

--- src/app/global/sideNavController.js
+++ src/app/global/sideNavController.js
@@ -4,14 +4,28 @@ angular
     .controller('sideNavController', function ($scope, $state, authService) {
         $scope.$state = $state;
         $scope.params = $state.params;
+        $scope.orgs = [];
 
-        if ($state.includes('backend.org')) {
-            authService.getUserProfile().then(function (userProfile) {
-                if (!userProfile.organizations || !($state.params.orgId in userProfile.organizations)) {
-                    return;
-                }
+        authService.getUserProfile().then(function (userProfile) {
+            if (!userProfile.organizations) {
+                return;
+            }
 
+            if ($state.includes('backend.org') && ($state.params.orgId in userProfile.organizations)) {
                 $scope.orgProfile = userProfile.organizations[$state.params.orgId];
-            });
-        }
+            }
+            else {
+                var orgs = [];
+                for (var orgId in userProfile.organizations) {
+                    if (userProfile.organizations.hasOwnProperty(orgId)) {
+                        orgs.push(userProfile.organizations[orgId]);
+                    }
+                }
+                $scope.orgs = orgs;
+            }
+        });
+
+        $scope.viewOrganization = function (id) {
+            $state.go('backend.org.dashboard', { orgId: id });
+        };
     });
