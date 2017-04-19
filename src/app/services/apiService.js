ede10677f9f9097951ecec6b910aa9a3ae37cab6
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-04-19 17:03:47 -0400

    includeShared for backwards compat APIs

--- src/app/services/apiService.js
+++ src/app/services/apiService.js
@@ -24,7 +24,7 @@
         _service.ciphers = $resource(_apiUri + '/ciphers/:id', {}, {
             get: { method: 'GET', params: { id: '@id' } },
             getFullDetails: { url: _apiUri + '/ciphers/:id/full-details', method: 'GET', params: { id: '@id' } },
-            list: { method: 'GET', params: { includeFolders: false } },
+            list: { method: 'GET', params: { includeFolders: false, includeShared: true } },
             listDetails: { url: _apiUri + '/ciphers/details', method: 'GET', params: {} },
             listOrganizationDetails: { url: _apiUri + '/ciphers/organization-details', method: 'GET', params: {} },
             'import': { url: _apiUri + '/ciphers/import', method: 'POST', params: {} },
