489b93d5df6058a4d2f52b86c987863f49f62d75
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-05-20 08:55:43 -0400

    fix lint errors

--- src/app/vault/vaultShareLoginController.js
+++ src/app/vault/vaultShareLoginController.js
@@ -74,8 +74,8 @@
             var collections = {};
             if ($event.target.checked) {
                 for (var i = 0; i < $scope.collections.length; i++) {
-                    if ($scope.model.organizationId && $scope.collections[i].organizationId === $scope.model.organizationId
-                        && !$scope.collections[i].readOnly) {
+                    if ($scope.model.organizationId && $scope.collections[i].organizationId === $scope.model.organizationId &&
+                        !$scope.collections[i].readOnly) {
                         collections[$scope.collections[i].id] = true;
                     }
                 }
