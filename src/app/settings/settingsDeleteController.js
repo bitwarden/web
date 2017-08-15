2772bffd0902c354c9bfb858fcd91568ba7b4f01
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-08-15 08:24:14 -0400

    qr code size and clean token on delete

--- src/app/settings/settingsDeleteController.js
+++ src/app/settings/settingsDeleteController.js
@@ -2,16 +2,22 @@
     .module('bit.settings')
 
     .controller('settingsDeleteController', function ($scope, $state, apiService, $uibModalInstance, cryptoService,
-        authService, toastr, $analytics) {
+        authService, toastr, $analytics, tokenService) {
         $analytics.eventTrack('settingsDeleteController', { category: 'Modal' });
         $scope.submit = function (model) {
-            $scope.submitPromise = cryptoService.hashPassword(model.masterPassword).then(function (hash) {
+            var profile;
+
+            $scope.submitPromise = authService.getUserProfile().then(function (theProfile) {
+                profile = theProfile;
+                return cryptoService.hashPassword(model.masterPassword);
+            }).then(function (hash) {
                 return apiService.accounts.postDelete({
                     masterPasswordHash: hash
                 }).$promise;
             }).then(function () {
                 $uibModalInstance.dismiss('cancel');
                 authService.logOut();
+                tokenService.clearTwoFactorToken(profile.email);
                 $analytics.eventTrack('Deleted Account');
                 return $state.go('frontend.login.info');
             }).then(function () {
