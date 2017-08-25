7099b0579a6b740b3fa61a9df467a026677f6e1d
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-08-25 11:00:19 -0400

    named args to server

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 cp /etc/bitwarden/web/settings.js /app/js/settings.js
 cp /etc/bitwarden/web/app-id.json /app/app-id.json
-dotnet /bitwarden_server/Server.dll /app .
+dotnet /bitwarden_server/Server.dll /contentRoot=/app /webRoot=. /serveUnknown=false
