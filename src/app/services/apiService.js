4262e2cc1dd5ea7e6fb6b100ac5deb40ad5ee68b
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-09-14 09:34:29 -0400

    remove old qs params

--- src/app/services/apiService.js
+++ src/app/services/apiService.js
@@ -28,7 +28,7 @@
         _service.ciphers = $resource(_apiUri + '/ciphers/:id', {}, {
             get: { method: 'GET', params: { id: '@id' } },
             getDetails: { url: _apiUri + '/ciphers/:id/details', method: 'GET', params: { id: '@id' } },
-            list: { method: 'GET', params: { includeFolders: false, includeShared: true } },
+            list: { method: 'GET', params: {} },
             listDetails: { url: _apiUri + '/ciphers/details', method: 'GET', params: {} },
             listOrganizationDetails: { url: _apiUri + '/ciphers/organization-details', method: 'GET', params: {} },
             'import': { url: _apiUri + '/ciphers/import', method: 'POST', params: {} },
