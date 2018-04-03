08b2184e12dfd3303f392f4b8ce95a1d36ab8c89
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-04-02 21:22:30 -0400

    version bump

--- src/app/settings.js
+++ src/app/settings.js
@@ -1,2 +1,2 @@
 angular.module("bit")
-.constant("appSettings", {"apiUri":"/api","identityUri":"/identity","iconsUri":"https://icons.bitwarden.com","stripeKey":"pk_live_bpN0P37nMxrMQkcaHXtAybJk","braintreeKey":"production_qfbsv8kc_njj2zjtyngtjmbjd","selfHosted":false,"version":"1.25.0","environment":"Production"});
+.constant("appSettings", {"apiUri":"/api","identityUri":"/identity","iconsUri":"https://icons.bitwarden.com","stripeKey":"pk_live_bpN0P37nMxrMQkcaHXtAybJk","braintreeKey":"production_qfbsv8kc_njj2zjtyngtjmbjd","selfHosted":false,"version":"1.25.1","environment":"Production"});
