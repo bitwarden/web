d4d01cc1867aebf65ce161adce3240f210e0ea3d
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-06-08 17:37:39 -0400

    set frontend layout body class

--- src/app/layouts/frontend-layout.component.ts
+++ src/app/layouts/frontend-layout.component.ts
@@ -1,9 +1,19 @@
 import {
     Component,
+    OnDestroy,
+    OnInit,
 } from '@angular/core';
 
 @Component({
     selector: 'app-frontend-layout',
     templateUrl: 'frontend-layout.component.html',
 })
-export class FrontendLayoutComponent { }
+export class FrontendLayoutComponent implements OnInit, OnDestroy {
+    ngOnInit() {
+        document.body.classList.add('layout_frontend');
+    }
+
+    ngOnDestroy() {
+        document.body.classList.remove('layout_frontend');
+    }
+}
