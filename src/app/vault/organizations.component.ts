5bd9a9a81d5773e07d61ab35ac8b817c6e7ccbb3
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-06-11 10:26:02 -0400

    loaded state for orgs

--- src/app/vault/organizations.component.ts
+++ src/app/vault/organizations.component.ts
@@ -4,28 +4,28 @@ import {
     Output,
 } from '@angular/core';
 
-import { ToasterService } from 'angular2-toaster';
-import { Angulartics2 } from 'angulartics2';
-
 import { UserService } from 'jslib/abstractions/user.service';
+
+import { Organization } from 'jslib/models/domain/organization';
 
 @Component({
     selector: 'app-vault-organizations',
     templateUrl: 'organizations.component.html',
 })
 export class OrganizationsComponent {
-    @Output() onOrganizationClicked = new EventEmitter<any>();
-    organizations: any;
+    @Output() onOrganizationClicked = new EventEmitter<Organization>();
+    organizations: Organization[];
+    loaded: boolean = false;
 
-    constructor(private userService: UserService, private analytics: Angulartics2,
-        private toasterService: ToasterService) {
+    constructor(private userService: UserService) {
     }
 
     async load() {
         this.organizations = await this.userService.getAllOrganizations();
+        this.loaded = true;
     }
 
-    selectOrganization(o: any) {
-        this.onOrganizationClicked.emit(0);
+    selectOrganization(o: Organization) {
+        this.onOrganizationClicked.emit(o);
     }
 }
