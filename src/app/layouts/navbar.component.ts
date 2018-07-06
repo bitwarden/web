b8921713eb8056a6f6bb57c10f1f09b8563fd613
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-07-06 09:47:50 -0400

    nav dropdown menu styling

--- src/app/layouts/navbar.component.ts
+++ src/app/layouts/navbar.component.ts
@@ -1,19 +1,27 @@
 import {
     Component,
+    OnInit,
 } from '@angular/core';
 
 import { MessagingService } from 'jslib/abstractions/messaging.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
+import { UserService } from 'jslib/abstractions/user.service';
 
 @Component({
     selector: 'app-navbar',
     templateUrl: 'navbar.component.html',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
     selfHosted = false;
+    email: string;
 
-    constructor(private messagingService: MessagingService, private platformUtilsService: PlatformUtilsService) {
+    constructor(private messagingService: MessagingService, private platformUtilsService: PlatformUtilsService,
+        private userService: UserService) {
         this.selfHosted = this.platformUtilsService.isSelfHost();
+    }
+
+    async ngOnInit() {
+        this.email = await this.userService.getEmail();
     }
 
     lock() {
