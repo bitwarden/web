de43eb4a57bd5953f3725c73e68d17d2ca1d68e0
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-07-18 11:15:23 -0400

    clean gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 const gulp = require('gulp');
 const googleWebFonts = require('gulp-google-webfonts');
+const del = require('del');
 
-gulp.task('build', ['webfonts']);
+const paths = {
+    cssDir: './src/css/',
+};
 
-gulp.task('webfonts', () => {
+function clean() {
+    return del([paths.cssDir]);
+}
+
+function webfonts() {
     return gulp.src('./webfonts.list')
         .pipe(googleWebFonts({
             fontsDir: 'webfonts',
-            cssFilename: 'webfonts.css'
+            cssFilename: 'webfonts.css',
+            format: 'woff',
         }))
-        .pipe(gulp.dest('./src/css/'));
-});
+        .pipe(gulp.dest(paths.cssDir));
+};
+
+gulp.task('clean', clean);
+gulp.task('webfonts', ['clean'], webfonts);
+gulp.task('build', ['webfonts']);
