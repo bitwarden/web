f43fd34a8c01aab4d0e28f5deedd3bdbf2c2c127
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-07-19 14:33:33 -0400

    fallback to en-us

--- src/services/i18n.service.ts
+++ src/services/i18n.service.ts
@@ -2,7 +2,7 @@ import { I18nService as BaseI18nService } from 'jslib/services/i18n.service';
 
 export class I18nService extends BaseI18nService {
     constructor(systemLanguage: string, localesDirectory: string) {
-        super(systemLanguage, localesDirectory, async (formattedLocale: string) => {
+        super(systemLanguage || 'en-US', localesDirectory, async (formattedLocale: string) => {
             const filePath = this.localesDirectory + '/' + formattedLocale + '/messages.json?cache=' +
                 process.env.CACHE_TAG;
             const localesResult = await fetch(filePath);
