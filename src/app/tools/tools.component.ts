c182d874af50bd9674d0cea1e9a469abb3804661
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-12-12 09:45:50 -0500

    premium labels for reports section

--- src/app/tools/tools.component.ts
+++ src/app/tools/tools.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    OnInit,
+} from '@angular/core';
+
+import { MessagingService } from 'jslib/abstractions/messaging.service';
+import { UserService } from 'jslib/abstractions/user.service';
 
 @Component({
     selector: 'app-tools',
     templateUrl: 'tools.component.html',
 })
-export class ToolsComponent { }
+export class ToolsComponent implements OnInit {
+    canAccessPremium = false;
+
+    constructor(private userService: UserService, private messagingService: MessagingService) { }
+
+    async ngOnInit() {
+        this.canAccessPremium = await this.userService.canAccessPremium();
+    }
+
+    async premiumRequired() {
+        if (!this.canAccessPremium) {
+            this.messagingService.send('premiumRequired');
+            return;
+        }
+    }
+}
