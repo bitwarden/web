e6cec93f2cb504f17900063067fcdc25e4789e08
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-01-25 15:05:22 -0500

    postinstall task for fixing sweetalert typings

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const package = require('./package.json');
 const fs = require('fs');
 
 const paths = {
+    node_modules: './node_modules/',
     src: './src/',
     build: './build/',
     cssDir: './src/css/',
@@ -29,8 +30,17 @@ function version(cb) {
     cb();
 }
 
+// ref: https://github.com/t4t5/sweetalert/issues/890
+function fixSweetAlert(cb) {
+    fs.writeFileSync(paths.node_modules + 'sweetalert/typings/sweetalert.d.ts',
+        'import swal, { SweetAlert } from "./core";export default swal;export as namespace swal;');
+    cb();
+}
+
 exports.clean = clean;
 exports.webfonts = gulp.series(clean, webfonts);
 exports.prebuild = gulp.series(clean, webfonts);
 exports.version = version;
 exports.postdist = version;
+exports.fixSweetAlert = fixSweetAlert;
+exports.postinstall = fixSweetAlert;
