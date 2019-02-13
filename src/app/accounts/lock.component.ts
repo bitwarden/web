259725882a403b8489fb782a3608353fd4a42595
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-02-13 15:32:41 -0500

    remembear csv importer

--- src/app/accounts/lock.component.ts
+++ src/app/accounts/lock.component.ts
@@ -3,8 +3,10 @@ import { Router } from '@angular/router';
 
 import { CryptoService } from 'jslib/abstractions/crypto.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
+import { LockService } from 'jslib/abstractions/lock.service';
 import { MessagingService } from 'jslib/abstractions/messaging.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
+import { StorageService } from 'jslib/abstractions/storage.service';
 import { UserService } from 'jslib/abstractions/user.service';
 
 import { RouterService } from '../services/router.service';
@@ -19,8 +21,10 @@ export class LockComponent extends BaseLockComponent {
     constructor(router: Router, i18nService: I18nService,
         platformUtilsService: PlatformUtilsService, messagingService: MessagingService,
         userService: UserService, cryptoService: CryptoService,
+        storageService: StorageService, lockService: LockService,
         private routerService: RouterService) {
-        super(router, i18nService, platformUtilsService, messagingService, userService, cryptoService);
+        super(router, i18nService, platformUtilsService, messagingService, userService, cryptoService,
+            storageService, lockService);
     }
 
     async ngOnInit() {
