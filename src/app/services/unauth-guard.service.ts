961954364a80b132acacdd505638aaaec5e13681
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-02-13 21:55:11 -0500

    move to lock service is locked

--- src/app/services/unauth-guard.service.ts
+++ src/app/services/unauth-guard.service.ts
@@ -4,19 +4,19 @@ import {
     Router,
 } from '@angular/router';
 
-import { CryptoService } from 'jslib/abstractions/crypto.service';
+import { LockService } from 'jslib/abstractions/lock.service';
 import { UserService } from 'jslib/abstractions/user.service';
 
 @Injectable()
 export class UnauthGuardService implements CanActivate {
-    constructor(private cryptoService: CryptoService, private userService: UserService,
+    constructor(private lockService: LockService, private userService: UserService,
         private router: Router) { }
 
     async canActivate() {
         const isAuthed = await this.userService.isAuthenticated();
         if (isAuthed) {
-            const hasKey = await this.cryptoService.hasKey();
-            if (!hasKey) {
+            const locked = await this.lockService.isLocked();
+            if (locked) {
                 this.router.navigate(['lock']);
             } else {
                 this.router.navigate(['vault']);
