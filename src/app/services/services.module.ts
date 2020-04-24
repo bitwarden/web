5076cb7e2487399da430bbd926152c34f4688a50
Vincent Salucci <vincesalucci21@gmail.com>
2020-04-24 16:34:21 -0500

    Added warning dialog for log out timeout action

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -142,18 +142,18 @@ export function initFactory(): Function {
                 'https://notifications.bitwarden.com'; // window.location.origin + '/notifications';
         }
         apiService.setUrls({
-            base: isDev ? null : window.location.origin,
-            api: isDev ? 'http://localhost:4000' : null,
-            identity: isDev ? 'http://localhost:33656' : null,
-            events: isDev ? 'http://localhost:46273' : null,
+            // base: isDev ? null : window.location.origin,
+            // api: isDev ? 'http://localhost:4000' : null,
+            // identity: isDev ? 'http://localhost:33656' : null,
+            // events: isDev ? 'http://localhost:46273' : null,
 
             // Uncomment these (and comment out the above) if you want to target production
             // servers for local development.
 
-            // base: null,
-            // api: 'https://api.bitwarden.com',
-            // identity: 'https://identity.bitwarden.com',
-            // events: 'https://events.bitwarden.com',
+            base: null,
+            api: 'https://api.bitwarden.com',
+            identity: 'https://identity.bitwarden.com',
+            events: 'https://events.bitwarden.com',
         });
         setTimeout(() => notificationsService.init(environmentService), 3000);
 
