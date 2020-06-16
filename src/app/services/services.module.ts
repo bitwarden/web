43090648048be2b803017baf0313ab628c950dd6
Vincent Salucci <26154748+vincentsalucci@users.noreply.github.com>
2020-06-16 09:35:25 -0500

    [Enterprise] Added environment checks (#559)
    * Update jslib (2b6657a -> 28d21ca)
    
    * Environment variable checks

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -140,6 +140,8 @@ export function initFactory(): Function {
         } else {
             environmentService.notificationsUrl = isDev ? 'http://localhost:61840' :
                 'https://notifications.bitwarden.com'; // window.location.origin + '/notifications';
+            environmentService.enterpriseUrl = isDev ? 'http://localhost:61840' :
+                'https://enterprise.bitwarden.com'; // window.location.origin + '/enterprise';
         }
         apiService.setUrls({
             base: isDev ? null : window.location.origin,
