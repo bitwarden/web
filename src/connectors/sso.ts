4461b0af66c8caa0fc560e020c702a1b4eee7524
Addison Beck <addisonbeck1@gmail.com>
2020-08-19 17:29:28 -0400

    implemented linking existing user to sso

--- src/connectors/sso.ts
+++ src/connectors/sso.ts
@@ -1,10 +1,20 @@
-﻿// tslint:disable-next-line
+﻿import { stat } from 'fs';
+
+// tslint:disable-next-line
 require('./sso.scss');
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const code = getQsParam('code');
     const state = getQsParam('state');
-    window.location.href = window.location.origin + '/#/sso?code=' + code + '&state=' + state;
+
+    // Match any characters between "_returnUri='" and the next "'"
+    const returnUri = extractFromRegex(state, '(?<=_returnUri=\')(.*)(?=\')');
+    if (returnUri) {
+        window.location.href = window.location.origin + `/#${returnUri}`;
+    } else {
+        window.location.href = window.location.origin + '/#/sso?code=' + code + '&state=' + state;
+    }
+
 });
 
 function getQsParam(name: string) {
@@ -21,4 +31,15 @@ function getQsParam(name: string) {
     }
 
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function extractFromRegex(s: string, regexString: string) {
+    const regex = new RegExp(regexString);
+    const results = regex.exec(s);
+
+    if (!results) {
+        return null;
+    }
+
+    return results[0];
 }
