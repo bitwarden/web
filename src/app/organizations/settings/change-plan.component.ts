8eead954f2fe24c0058286e9b1f34970e4a026fb
Addison Beck <addisonbeck1@gmail.com>
2020-09-18 13:33:21 -0400

    fixed a broken default selection for plan upgrades

--- src/app/organizations/settings/change-plan.component.ts
+++ src/app/organizations/settings/change-plan.component.ts
@@ -8,6 +8,9 @@ import {
 import { ApiService } from 'jslib/abstractions/api.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
 
+import { PlanType } from 'jslib/enums/planType';
+import { ProductType } from 'jslib/enums/productType';
+
 @Component({
     selector: 'app-change-plan',
     templateUrl: 'change-plan.component.html',
@@ -18,6 +21,8 @@ export class ChangePlanComponent {
     @Output() onCanceled = new EventEmitter();
 
     formPromise: Promise<any>;
+    defaultUpgradePlan: PlanType = PlanType.FamiliesAnnually;
+    defaultUpgradeProduct: ProductType = ProductType.Families
 
     constructor(private apiService: ApiService, private platformUtilsService: PlatformUtilsService) { }
 
