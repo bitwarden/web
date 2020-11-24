4625b44703f4c522b4d6182686884367f5dc22e7
Matt Gibson <mgibson@bitwarden.com>
2020-11-24 11:29:04 -0600

    WIP: dirty fix to SSO web vs browser redirect logic split (#719)
    * WIP: dirty fix to SSO web vs browser redirect logic split
    
    * Use includes for clientId identification
    
    routing determination more robust to future state string changes
    
    Co-authored-by: Addison Beck <abeck@bitwarden.com>
    
    Co-authored-by: Matt Gibson <mdgibson@Matts-MBP.lan>
    Co-authored-by: Addison Beck <abeck@bitwarden.com>

--- src/connectors/sso.ts
+++ src/connectors/sso.ts
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const code = getQsParam('code');
     const state = getQsParam('state');
 
-    if (state != null && state.endsWith(':clientId=browser')) {
+    if (state != null && state.includes(':clientId=browser')) {
         initiateBrowserSso(code, state);
     } else {
         window.location.href = window.location.origin + '/#/sso?code=' + code + '&state=' + state;
