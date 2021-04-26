1f1a5b721b27718f8f3dc45e543a03e080484d11
Hinton <oscar@oscarhinton.com>
2021-04-26 15:25:15 +0200

    Cleanup tsconfig, webpack, add jslib-angular to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -227,10 +227,6 @@ const webpackConfig = {
     },
     resolve: {
         extensions: ['.ts', '.js'],
-        alias: {
-            jslib: path.join(__dirname, 'jslib/src'),
-            tldjs: path.join(__dirname, 'jslib/src/misc/tldjs.noop'),
-        },
         symlinks: false,
         modules: [path.resolve('node_modules')],
     },
