4a61f0ac04c73511fc95e0cba55d3196f592f1d7
Oscar Hinton <oscar@oscarhinton.com>
2021-05-05 09:46:14 +0200

    Cleanup tsconfig (#954)
    * Cleanup tsconfig
    
    * Removed dummy module

--- src/services/webPlatformUtils.service.ts
+++ src/services/webPlatformUtils.service.ts
@@ -1,4 +1,4 @@
-import Swal, { SweetAlertIcon } from 'sweetalert2/dist/sweetalert2.js';
+import Swal, { SweetAlertIcon } from 'sweetalert2';
 
 import { DeviceType } from 'jslib/enums/deviceType';
 
