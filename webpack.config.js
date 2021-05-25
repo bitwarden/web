bdae03b25a9e5a3e41aaefde6e7ca27d1c5d2cc6
Danny Murphy <6512845+dltmurphy@users.noreply.github.com>
2021-05-25 18:09:33 +0100

    Add Paypal Container and Loading svg file

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ const moduleRules = [
     },
     {
         test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        exclude: /loading.svg/,
+        exclude: /loading(|-white).svg/,
         use: [{
             loader: 'file-loader',
             options: {
