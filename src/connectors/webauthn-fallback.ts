ad1c8d4b6d276f30045ea309e28e67fbe0d1c924
Matt Gibson <mgibson@bitwarden.com>
2021-07-20 16:32:02 -0400

    Handle parameter versions in webauthn

--- src/connectors/webauthn-fallback.ts
+++ src/connectors/webauthn-fallback.ts
@@ -4,15 +4,67 @@ import { buildDataString, parseWebauthnJson } from './common-webauthn';
 // tslint:disable-next-line
 require('./webauthn.scss');
 
+let parsed = false;
+let webauthnJson: any;
 let parentUrl: string = null;
 let parentOrigin: string = null;
 let sentSuccess = false;
+let locale: string = 'en';
 
 let locales: any = {};
 
+function parseParameters() {
+    if (parsed) {
+        return;
+    }
+
+    parentUrl = getQsParam('parent');
+    if (!parentUrl) {
+        error('No parent.');
+        return;
+    } else {
+        parentUrl = decodeURIComponent(parentUrl);
+        parentOrigin = new URL(parentUrl).origin;
+    }
+
+    locale = getQsParam('locale').replace('-', '_');
+
+    const version = getQsParam('v');
+
+    if (version === '1') {
+        parseParametersV1();
+    } else {
+        parseParametersV2();
+    }
+    parsed = true;
+}
+
+function parseParametersV1() {
+    const data = getQsParam('data');
+    if (!data) {
+        error('No data.');
+        return;
+    }
+
+    webauthnJson = b64Decode(data);
+}
+
+function parseParametersV2() {
+    let dataObj: { data: any, btnText: string; } = null;
+    try {
+        dataObj = JSON.parse(b64Decode(getQsParam('data')));
+    }
+    catch (e) {
+        error('Cannot parse data.');
+        return;
+    }
+
+    webauthnJson = dataObj.data;
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
 
-    const locale = getQsParam('locale').replace('-', '_');
+    parseParameters();
     try {
         locales = await loadLocales(locale);
     } catch {
@@ -54,25 +106,15 @@ function start() {
         return;
     }
 
-    const data = getQsParam('data');
-    if (!data) {
+    parseParameters();
+    if (!webauthnJson) {
         error('No data.');
         return;
     }
 
-    parentUrl = getQsParam('parent');
-    if (!parentUrl) {
-        error('No parent.');
-        return;
-    } else {
-        parentUrl = decodeURIComponent(parentUrl);
-        parentOrigin = new URL(parentUrl).origin;
-    }
-
     let json: any;
     try {
-        const jsonString = b64Decode(data);
-        json = parseWebauthnJson(jsonString);
+        json = parseWebauthnJson(webauthnJson);
     }
     catch (e) {
         error('Cannot parse data.');
--- src/connectors/webauthn.ts
+++ src/connectors/webauthn.ts
@@ -7,16 +7,17 @@ require('./webauthn.scss');
 document.addEventListener('DOMContentLoaded', () => {
     init();
 
-    parseDataParameter();
-    const text = data.btnText;
-    if (text) {
+    parseParameters();
+    if (btnText) {
         const button = document.getElementById('webauthn-button');
-        button.innerText = decodeURI(text);
+        button.innerText = decodeURI(btnText);
         button.onclick = executeWebAuthn;
     }
 });
 
-let data: any;
+let parsed = false;
+let webauthnJson: any;
+let btnText: string = null;
 let parentUrl: string = null;
 let parentOrigin: string = null;
 let stopWebAuthn = false;
@@ -29,31 +30,8 @@ function init() {
     info('ready');
 }
 
-function parseDataParameter() {
-    if (data) {
-        return;
-    }
-
-    try {
-        data = JSON.parse(b64Decode(getQsParam('data')));
-    }
-    catch (e) {
-        error('Cannot parse data.');
-        return;
-    }
-}
-
-function start() {
-    sentSuccess = false;
-
-    if (!('credentials' in navigator)) {
-        error('WebAuthn is not supported in this browser.');
-        return;
-    }
-
-    parseDataParameter();
-    if (!data) {
-        error('No data.');
+function parseParameters() {
+    if (parsed) {
         return;
     }
 
@@ -66,9 +44,57 @@ function start() {
         parentOrigin = new URL(parentUrl).origin;
     }
 
+    const version = getQsParam('v');
+
+    if (version === '1') {
+        parseParametersV1();
+    } else {
+        parseParametersV2();
+    }
+    parsed = true;
+}
+
+function parseParametersV1() {
+    const data = getQsParam('data');
+    if (!data) {
+        error('No data.');
+        return;
+    }
+
+    webauthnJson = b64Decode(data);
+    btnText = getQsParam('btnText');
+}
+
+function parseParametersV2() {
+    let dataObj: { data: any, btnText: string; } = null;
     try {
-        const jsonString = data.data;
-        obj = parseWebauthnJson(jsonString);
+        dataObj = JSON.parse(b64Decode(getQsParam('data')));
+    }
+    catch (e) {
+        error('Cannot parse data.');
+        return;
+    }
+
+    webauthnJson = dataObj.data;
+    btnText = dataObj.btnText;
+}
+
+function start() {
+    sentSuccess = false;
+
+    if (!('credentials' in navigator)) {
+        error('WebAuthn is not supported in this browser.');
+        return;
+    }
+
+    parseParameters();
+    if (!webauthnJson) {
+        error('No data.');
+        return;
+    }
+
+    try {
+        obj = parseWebauthnJson(webauthnJson);
     }
     catch (e) {
         error('Cannot parse webauthn data.');
