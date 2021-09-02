6e45d51f38330cdce396851bb958bcf0b0a1344c
Arun Pattni <me@arunpattni.com>
2021-09-02 19:51:44 +0100

    Fix issues

--- src/app/tools/inactive-two-factor-report.component.ts
+++ src/app/tools/inactive-two-factor-report.component.ts
@@ -54,12 +54,18 @@ export class InactiveTwoFactorReportComponent extends CipherReportComponent impl
                     const u = c.login.uris[i];
                     if (u.uri != null && u.uri !== '') {
                         const hostname = Utils.getHostname(u.uri);
+                        const domain = Utils.getDomain(u.uri);
                         if (hostname != null && this.services.has(hostname)) {
                             if (this.services.get(hostname) != null) {
                                 docs.set(c.id, this.services.get(hostname));
                             }
                             inactive2faCiphers.push(c);
                             break;
+                        } else if (domain != null && this.services.has(domain)){
+                            if (this.services.get(domain) != null) {
+                                docs.set(c.id, this.services.get(domain));
+                            }
+                            inactive2faCiphers.push(c);
                         }
                     }
                 }
@@ -89,10 +95,10 @@ export class InactiveTwoFactorReportComponent extends CipherReportComponent impl
                 if (serviceData.documentation != null) {
                     if (serviceData['additional-domains'] != null) {
                         for (const additionalDomain of serviceData['additional-domains']) {
-                            this.services.set(additionalDomain, serviceData.documentation);
+                            this.services.set(Utils.getHostname(additionalDomain), serviceData.documentation);
                         }
                     }
-                    this.services.set(serviceData.domain, serviceData.documentation);
+                    this.services.set(Utils.getHostname(serviceData.domain), serviceData.documentation);
                 }
             }
         }
