59645b9afa18d9b3a3c3f94f57c2e512d85e7a7f
Thomas Rittson <trittson@bitwarden.com>
2021-10-28 14:24:01 +1000

    Add OTP to delete organization

--- src/app/organizations/settings/delete-organization.component.ts
+++ src/app/organizations/settings/delete-organization.component.ts
@@ -3,10 +3,14 @@ import { Router } from '@angular/router';
 
 import { ToasterService } from 'angular2-toaster';
 
+import { Verification } from 'jslib-angular/components/verify-master-password.component';
+
 import { ApiService } from 'jslib-common/abstractions/api.service';
 import { CryptoService } from 'jslib-common/abstractions/crypto.service';
 import { I18nService } from 'jslib-common/abstractions/i18n.service';
 import { LogService } from 'jslib-common/abstractions/log.service';
+
+import { VerificationType } from 'jslib-common/enums/verificationType';
 
 import { PasswordVerificationRequest } from 'jslib-common/models/request/passwordVerificationRequest';
 
@@ -17,7 +21,7 @@ import { PasswordVerificationRequest } from 'jslib-common/models/request/passwor
 export class DeleteOrganizationComponent {
     organizationId: string;
 
-    masterPassword: string;
+    masterPassword: Verification;
     formPromise: Promise<any>;
 
     constructor(private apiService: ApiService, private i18nService: I18nService,
@@ -25,14 +29,19 @@ export class DeleteOrganizationComponent {
         private router: Router, private logService: LogService) { }
 
     async submit() {
-        if (this.masterPassword == null || this.masterPassword === '') {
+        if (this.masterPassword?.secret == null || this.masterPassword.secret === '') {
             this.toasterService.popAsync('error', this.i18nService.t('errorOccurred'),
                 this.i18nService.t('masterPassRequired'));
             return;
         }
 
         const request = new PasswordVerificationRequest();
-        request.masterPasswordHash = await this.cryptoService.hashPassword(this.masterPassword, null);
+        if (this.masterPassword.type === VerificationType.MasterPassword) {
+            request.masterPasswordHash = await this.cryptoService.hashPassword(this.masterPassword.secret, null);
+        } else {
+            request.otp = this.masterPassword.secret;
+        }
+
         try {
             this.formPromise = this.apiService.deleteOrganization(this.organizationId, request);
             await this.formPromise;
