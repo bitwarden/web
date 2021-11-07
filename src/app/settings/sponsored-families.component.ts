e0b6e3cfdc13263bf294be1fa505ae95d1007bbd
Justin Baur <admin@justinbaur.com>
2021-11-07 11:47:15 -0500

    Switch to new figma design

--- src/app/settings/sponsored-families.component.ts
+++ src/app/settings/sponsored-families.component.ts
@@ -10,44 +10,59 @@ import { Organization } from 'jslib-common/models/domain/organization';
     templateUrl: 'sponsored-families.component.html',
 })
 export class SponsoredFamiliesComponent implements OnInit {
-    availableSponsoshipOrgs: Organization[];
-    activeSponsorshipsOrgs: Organization[];
-    selectedSponsorshipOrgId: string = "";
-    sponsorshipEmail: string  = "";
+    availableSponsorshipOrgs: Organization[];
+    // TODO: I think this will be a different model
+    activeSponsorshipOrgs: Organization[];
+    selectedSponsorshipOrgId: string = '';
+    sponsorshipEmail: string  = '';
 
     // Conditional display properties
-    moreThanOneOrgSponsored: boolean;
+    anyActiveSponsorships: boolean;
     moreThanOneOrgAvailable: boolean;
     anyOrgsAvailable: boolean;
 
     formPromise: Promise<any>;
 
-    public constructor(private userService: UserService) { }
+    constructor(private userService: UserService) { }
 
-    public async ngOnInit() {
-        
+    async ngOnInit() {
+
         const allOrgs = await this.userService.getAllOrganizations();
 
         // TODO: Filter out organizations that can't do sponsorships
-        this.availableSponsoshipOrgs = allOrgs.filter(org => true);
-        this.activeSponsorshipsOrgs = allOrgs.filter(org => true);
+        this.availableSponsorshipOrgs = allOrgs.filter(org => true);
 
-        // Auto select the org if there is only one
-        if (this.availableSponsoshipOrgs.length === 1) {
-            this.selectedSponsorshipOrgId = this.availableSponsoshipOrgs[0].id;
-            this.moreThanOneOrgAvailable = false;
+        // TODO: I don't think this will be a filter but it's own call
+        this.activeSponsorshipOrgs = allOrgs.filter(org => true);
+
+        if (this.availableSponsorshipOrgs.length > 0) {
             this.anyOrgsAvailable = true;
+            this.moreThanOneOrgAvailable = this.availableSponsorshipOrgs.length > 1;
+
+            if (this.availableSponsorshipOrgs.length === 1) {
+                this.selectedSponsorshipOrgId = this.availableSponsorshipOrgs[0].id;
+            }
         }
 
-        if (this.activeSponsorshipsOrgs.length > 1) {
-            this.moreThanOneOrgSponsored = true;
+        if (this.activeSponsorshipOrgs.length > 0) {
+            this.anyActiveSponsorships = true;
         }
 
-        console.log(this.availableSponsoshipOrgs);
-        console.log(this.activeSponsorshipsOrgs);
+        console.log({anyOrgsAvailable: this.anyOrgsAvailable,
+            moreThanOneOrgAvailable: this.moreThanOneOrgAvailable,
+            anyActiveSponsorships: this.anyActiveSponsorships});
     }
 
-    public async submit() {
+    async removeSponsorship(org: Organization) {
+        console.log('Remove Sponsorship: ', org);
+    }
+
+    async submit() {
         // TODO: Validate form and submit
+        // It seems like email validation is done on the server side
+        console.log('Form Items: ', { 
+            selectedSponsorshipOrgId: this.selectedSponsorshipOrgId, 
+            sponsorshipEmail: this.sponsorshipEmail,
+        });
     }
 }
