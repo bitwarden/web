ad01347d0cc32633733c086f131c28c4eec3dec2
Justin Baur <admin@justinbaur.com>
2021-11-07 20:37:02 -0500

    Add calls to server

--- src/app/settings/sponsored-families.component.ts
+++ src/app/settings/sponsored-families.component.ts
@@ -2,7 +2,13 @@ import {
     Component,
     OnInit,
 } from '@angular/core';
+import { ToasterService } from 'angular2-toaster';
+import { ApiService } from 'jslib-common/abstractions/api.service';
+import { I18nService } from 'jslib-common/abstractions/i18n.service';
+import { LogService } from 'jslib-common/abstractions/log.service';
+import { PlatformUtilsService } from 'jslib-common/abstractions/platformUtils.service';
 import { UserService } from 'jslib-common/abstractions/user.service';
+import { PlanSponsorshipType } from 'jslib-common/enums/planSponsorshipType';
 import { Organization } from 'jslib-common/models/domain/organization';
 
 @Component({
@@ -23,16 +29,21 @@ export class SponsoredFamiliesComponent implements OnInit {
 
     formPromise: Promise<any>;
 
-    constructor(private userService: UserService) { }
+    constructor(private userService: UserService, private apiService: ApiService,
+        private platformUtilsService: PlatformUtilsService, private i18nService: I18nService,
+        private toasterService: ToasterService, private logService: LogService) { }
 
     async ngOnInit() {
+        await this.load();
+    }
 
+    private async load() {
         const allOrgs = await this.userService.getAllOrganizations();
 
         // TODO: Filter out organizations that can't do sponsorships
         this.availableSponsorshipOrgs = allOrgs.filter(org => true);
 
-        // TODO: I don't think this will be a filter but it's own call
+        // TODO: I don't think this will be a filter but its own call
         this.activeSponsorshipOrgs = allOrgs.filter(org => true);
 
         if (this.availableSponsorshipOrgs.length > 0) {
@@ -47,22 +58,32 @@ export class SponsoredFamiliesComponent implements OnInit {
         if (this.activeSponsorshipOrgs.length > 0) {
             this.anyActiveSponsorships = true;
         }
-
-        console.log({anyOrgsAvailable: this.anyOrgsAvailable,
-            moreThanOneOrgAvailable: this.moreThanOneOrgAvailable,
-            anyActiveSponsorships: this.anyActiveSponsorships});
     }
 
     async removeSponsorship(org: Organization) {
-        console.log('Remove Sponsorship: ', org);
+        const isConfirmed = await this.platformUtilsService.showDialog(
+            'Are you sure you want to remove this sponsorship?', 'test@email.com',
+            this.i18nService.t('yes'), this.i18nService.t('no'), 'warning');
+        if (!isConfirmed) {
+            return;
+        }
+
+        try {
+            // TODO: Remove sponsorship
+
+            this.toasterService.popAsync('success', null, 'Sponsorship Removed');
+            this.load();
+        } catch (e) {
+            this.logService.error(e);
+        }
+
     }
 
     async submit() {
-        // TODO: Validate form and submit
-        // It seems like email validation is done on the server side
-        console.log('Form Items: ', { 
-            selectedSponsorshipOrgId: this.selectedSponsorshipOrgId, 
-            sponsorshipEmail: this.sponsorshipEmail,
+        await this.apiService.postCreateSponsorship(this.selectedSponsorshipOrgId, {
+            sponsoredEmail: this.sponsorshipEmail,
+            planSponsorshipType: PlanSponsorshipType.FamiliesForEnterprise,
+            organizationUserId: await this.userService.getUserId(),
         });
     }
 }
