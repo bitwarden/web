d4098b4b282075765eea5472585e78b8edf59be1
Hinton <oscar@oscarhinton.com>
2022-01-06 16:22:08 +0100

    Optimize all png images

--- scripts/optimize.js
+++ scripts/optimize.js
@@ -1,12 +1,14 @@
 const child_process = require("child_process");
 const path = require("path");
 
-const [img] = process.argv.slice(2);
+const images = process.argv.slice(2);
 
-switch (img.split(".").pop()) {
-  case "png":
-    child_process.execSync(
-      `npx @squoosh/cli --oxipng '{}' --output-dir "${path.dirname(img)}" "${img}"`
-    );
-    break;
-}
+images.forEach((img) => {
+  switch (img.split(".").pop()) {
+    case "png":
+      child_process.execSync(
+        `npx @squoosh/cli --oxipng '{}' --output-dir "${path.dirname(img)}" "${img}"`
+      );
+      break;
+  }
+});
