9dca024a685759f4c0e7b8c94be6e925d8d97e6a
Hinton <oscar@oscarhinton.com>
2022-02-10 14:32:28 +0100

    Add color variants of success, danger, warning and info

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,10 +22,26 @@ module.exports = {
         700: "var(--color-secondary-700)",
         900: "var(--color-secondary-900)",
       },
-      success: "--color-success",
-      danger: "--color-danger",
-      warning: "--color-warning",
-      info: "--color-info",
+      success: {
+        300: "var(--color-success-300)",
+        500: "var(--color-success-500)",
+        700: "var(--color-success-700)",
+      },
+      danger: {
+        300: "var(--color-danger-300)",
+        500: "var(--color-danger-500)",
+        700: "var(--color-danger-700)",
+      },
+      warning: {
+        300: "var(--color-warning-300)",
+        500: "var(--color-warning-500)",
+        700: "var(--color-warning-700)",
+      },
+      info: {
+        300: "var(--color-info-300)",
+        500: "var(--color-info-500)",
+        700: "var(--color-info-700)",
+      },
 
       "text-body": "--color-text-body",
       "text-muted": "--color-text-muted",
