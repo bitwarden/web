2fe1b9532c020b158c5a863895b8895c41b43e0a
Chad Scharf <3904944+cscharf@users.noreply.github.com>
2022-02-25 17:23:47 -0500

    Remove artifact binding from SSO config UI

--- bitwarden_license/src/app/organizations/manage/sso.component.ts
+++ bitwarden_license/src/app/organizations/manage/sso.component.ts
@@ -65,7 +65,6 @@ export class SsoComponent implements OnInit {
     idpBindingType: [],
     idpSingleSignOnServiceUrl: [],
     idpSingleLogoutServiceUrl: [],
-    idpArtifactResolutionServiceUrl: [],
     idpX509PublicCert: [],
     idpOutboundSigningAlgorithm: [],
     idpAllowUnsolicitedAuthnResponse: [],
