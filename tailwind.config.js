4032d819e596f19aee2821eca1b827592b094fb2
Hinton <oscar@oscarhinton.com>
2022-02-25 18:05:35 +0100

    Update the colors to match the figma spec

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,43 +15,34 @@ module.exports = {
         700: "var(--color-primary-700)",
       },
       secondary: {
-        200: "var(--color-secondary-200)",
+        100: "var(--color-secondary-100)",
         300: "var(--color-secondary-300)",
         500: "var(--color-secondary-500)",
         700: "var(--color-secondary-700)",
-        900: "var(--color-secondary-900)",
-        text: "var(--color-secondary-text)",
-        "text-hover": "var(--color-secondary-text-hover)",
       },
       success: {
-        300: "var(--color-success-300)",
         500: "var(--color-success-500)",
         700: "var(--color-success-700)",
       },
       danger: {
-        300: "var(--color-danger-300)",
         500: "var(--color-danger-500)",
         700: "var(--color-danger-700)",
       },
       warning: {
-        300: "var(--color-warning-300)",
         500: "var(--color-warning-500)",
         700: "var(--color-warning-700)",
       },
       info: {
-        300: "var(--color-info-300)",
         500: "var(--color-info-500)",
         700: "var(--color-info-700)",
       },
-      outline: {
-        background: "var(--color-outline-background)",
-        border: "var(--color-outline-border)",
-      },
-      "text-body": "--color-text-body",
-      "text-muted": "--color-text-muted",
-      "text-black50": "--color-text-black50",
-      "text-white50": "--color-text-white50",
     },
+    textColor: {
+      main: "var(--color-text-main)",
+      muted: "var(--color-text-muted)",
+      contrast: "var(--color-text-contrast)",
+      danger: "var(--color-danger-500)",
+    }
   },
   plugins: [],
 };
