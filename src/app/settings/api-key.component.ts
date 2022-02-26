1258989f8abed85f06e498e2552d8568e620bd59
Matt Gibson <mgibson@bitwarden.com>
2022-02-26 13:28:24 -0500

    POC
    TODO:
    * encrypt key information with symm key
    * store symm key with server next to api key
    * Add toggle for enc access yes/no
    * build out storing multiple API keys
    * Add quick copy button to api key field
    * See about a more user-friendly api key encoding

--- src/app/settings/api-key.component.ts
+++ src/app/settings/api-key.component.ts
@@ -1,7 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
+import { CryptoService } from "jslib-common/abstractions/crypto.service";
 import { LogService } from "jslib-common/abstractions/log.service";
 import { UserVerificationService } from "jslib-common/abstractions/userVerification.service";
+import { Utils } from "jslib-common/misc/utils";
 import { SecretVerificationRequest } from "jslib-common/models/request/secretVerificationRequest";
 import { ApiKeyResponse } from "jslib-common/models/response/apiKeyResponse";
 import { Verification } from "jslib-common/types/verification";
@@ -10,7 +12,7 @@ import { Verification } from "jslib-common/types/verification";
   selector: "app-api-key",
   templateUrl: "api-key.component.html",
 })
-export class ApiKeyComponent {
+export class ApiKeyComponent implements OnInit {
   keyType: string;
   isRotation: boolean;
   postKey: (entityId: string, request: SecretVerificationRequest) => Promise<ApiKeyResponse>;
@@ -25,11 +27,19 @@ export class ApiKeyComponent {
   formPromise: Promise<ApiKeyResponse>;
   clientId: string;
   clientSecret: string;
+  clientKey: string;
+  clientLocalKeyHash: string;
 
   constructor(
+    private cryptoService: CryptoService,
     private userVerificationService: UserVerificationService,
     private logService: LogService
   ) {}
+
+  async ngOnInit(): Promise<void> {
+    this.clientKey = Utils.fromBufferToB64((await this.cryptoService.getKey()).key);
+    this.clientLocalKeyHash = await this.cryptoService.getKeyHash();
+  }
 
   async submit() {
     try {
@@ -42,5 +52,18 @@ export class ApiKeyComponent {
     } catch (e) {
       this.logService.error(e);
     }
+  }
+
+  get combinedApiKey() {
+    return Utils.fromUtf8ToB64(
+      JSON.stringify({
+        clientId: this.clientId,
+        clientSecret: this.clientSecret,
+        encClientEncInfo: JSON.stringify({
+          clientEncKey: this.clientKey,
+          clientLocalKeyHash: this.clientLocalKeyHash,
+        }),
+      })
+    );
   }
 }
