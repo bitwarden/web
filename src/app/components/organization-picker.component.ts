f140440903943872437510dd829da466e1094c49
Thomas Rittson <trittson@bitwarden.com>
2022-03-08 12:07:51 +1000

    WIP organization picker

--- src/app/components/organization-picker.component.ts
+++ src/app/components/organization-picker.component.ts
@@ -1,11 +1,45 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
+import { I18nService } from "jslib-common/abstractions/i18n.service";
+import { OrganizationService } from "jslib-common/abstractions/organization.service";
+import { Utils } from "jslib-common/misc/utils";
 import { Organization } from "jslib-common/models/domain/organization";
 
 @Component({
   selector: "app-organization-picker",
   templateUrl: "organization-picker.component.html",
 })
-export class OrganizationPickerComponent {
-  @Input() organization: Organization;
+export class OrganizationPickerComponent implements OnInit {
+  constructor(
+    private organizationService: OrganizationService,
+    private i18nService: I18nService,
+    private route: ActivatedRoute
+  ) {}
+
+  activeOrganizationId: string;
+  organizations: Organization[] = [];
+
+  collapsed = true;
+  loaded = false;
+
+  get activeOrganization() {
+    return this.organizations.find((org) => org.id == this.activeOrganizationId);
+  }
+
+  async ngOnInit() {
+    this.route.params.subscribe(async (params: any) => {
+      this.activeOrganizationId = params.organizationId;
+      await this.load();
+    });
+  }
+
+  async load() {
+    const orgs = await this.organizationService.getAll();
+    this.organizations = orgs
+      .filter((org) => org.isAdmin)
+      .sort(Utils.getSortFunction(this.i18nService, "name"));
+
+    this.loaded = true;
+  }
 }
