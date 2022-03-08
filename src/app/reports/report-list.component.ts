462f5c1acebe6cdcc0a85a929826ea8c38cc1e87
Hinton <oscar@oscarhinton.com>
2022-03-08 15:14:56 +0100

    Update text, use loop

--- src/app/reports/report-list.component.ts
+++ src/app/reports/report-list.component.ts
@@ -1,7 +1,39 @@
 import { Component } from "@angular/core";
 
+type ReportEntry = {
+  title: string;
+  description: string;
+};
+
 @Component({
   selector: "app-report-list",
   templateUrl: "report-list.component.html",
 })
-export class ReportListComponent {}
+export class ReportListComponent {
+  reports: ReportEntry[] = [
+    {
+      title: "exposedPasswordsReport",
+      description: "exposedPasswordsReportDesc",
+    },
+    {
+      title: "reusedPasswordsReport",
+      description: "reusedPasswordsReportDesc",
+    },
+    {
+      title: "weakPasswordsReport",
+      description: "weakPasswordsReportDesc",
+    },
+    {
+      title: "unsecuredWebsitesReport",
+      description: "unsecuredWebsitesReportDesc",
+    },
+    {
+      title: "inactive2faReport",
+      description: "inactive2faReportDesc",
+    },
+    {
+      title: "dataBreachReport",
+      description: "breachDesc",
+    },
+  ];
+}
