c46b0e216c672c82cb1e9c768f1d57550b5c46bc
Hinton <oscar@oscarhinton.com>
2022-03-08 18:07:36 +0100

    Fix linter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-undef, @typescript-eslint/no-var-requires */
 const config = require("./jslib/components/tailwind.config.base");
 
 module.exports = config;
