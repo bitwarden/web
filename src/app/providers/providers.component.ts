ac0dbfc76b4599e419d01b5b4411e895b7f4a30c
Thomas Rittson <trittson@bitwarden.com>
2022-03-09 14:18:11 +1000

    Remove unused imports

--- src/app/providers/providers.component.ts
+++ src/app/providers/providers.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { I18nService } from "jslib-common/abstractions/i18n.service";
 import { ProviderService } from "jslib-common/abstractions/provider.service";
--- src/app/settings/organizations.component.ts
+++ src/app/settings/organizations.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { ApiService } from "jslib-common/abstractions/api.service";
 import { CryptoService } from "jslib-common/abstractions/crypto.service";
