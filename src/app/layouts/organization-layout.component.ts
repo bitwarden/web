5d563a0c67e0627bbc8109133c9c2e2c6b14af62
Thomas Rittson <trittson@bitwarden.com>
2022-03-17 08:38:28 +1000

    Remove routing changes

--- src/app/layouts/organization-layout.component.ts
+++ src/app/layouts/organization-layout.component.ts
@@ -1,10 +1,8 @@
 import { Component, NgZone, OnDestroy, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
 
 import { BroadcasterService } from "jslib-common/abstractions/broadcaster.service";
-import { I18nService } from "jslib-common/abstractions/i18n.service";
 import { OrganizationService } from "jslib-common/abstractions/organization.service";
-import { Utils } from "jslib-common/misc/utils";
 import { Organization } from "jslib-common/models/domain/organization";
 
 const BroadcasterSubscriptionId = "OrganizationLayoutComponent";
@@ -22,9 +20,7 @@ export class OrganizationLayoutComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private organizationService: OrganizationService,
     private broadcasterService: BroadcasterService,
-    private ngZone: NgZone,
-    private i18nService: I18nService,
-    private router: Router
+    private ngZone: NgZone
   ) {}
 
   ngOnInit() {
@@ -49,15 +45,7 @@ export class OrganizationLayoutComponent implements OnInit, OnDestroy {
   }
 
   async load() {
-    if (this.organizationId != null) {
-      this.organization = await this.organizationService.get(this.organizationId);
-    } else {
-      const orgs = await this.organizationService.getAll();
-      const allowedOrgs = orgs
-        .filter((org) => org.isAdmin)
-        .sort(Utils.getSortFunction(this.i18nService, "name"));
-      this.router.navigate(["organizations", allowedOrgs[0].id]);
-    }
+    this.organization = await this.organizationService.get(this.organizationId);
   }
 
   get showMenuBar() {
