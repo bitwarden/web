f5143f64ddf06eaf2e785dbb861b309edf1295d8
Hinton <oscar@oscarhinton.com>
2022-03-18 11:20:25 +0100

    Undo premium badge changes

--- src/app/components/premium-badge.component.ts
+++ src/app/components/premium-badge.component.ts
@@ -1,33 +1,17 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 import { MessagingService } from "jslib-common/abstractions/messaging.service";
-import { StateService } from "jslib-common/abstractions/state.service";
 
 @Component({
   selector: "app-premium-badge",
   template: `
-    <button
-      bit-badge
-      badgeType="success"
-      *ngIf="condition"
-      appStopClick
-      (click)="premiumRequired()"
-      [attr.tabIndex]="tabindex"
-    >
-      {{ "premium" | i18n }}
-    </button>
+    <a href="#" appStopClick *appNotPremium (click)="premiumRequired()">
+      <bit-badge type="success">{{ "premium" | i18n }}</bit-badge>
+    </a>
   `,
 })
-export class PremiumBadgeComponent implements OnInit {
-  // Optional condition defaults to premium
-  @Input() condition: boolean;
-  @Input() tabindex?: number = null;
-
-  constructor(private stateService: StateService, private messagingService: MessagingService) {}
-
-  async ngOnInit() {
-    this.condition ??= !(await this.stateService.getCanAccessPremium());
-  }
+export class PremiumBadgeComponent {
+  constructor(private messagingService: MessagingService) {}
 
   premiumRequired() {
     this.messagingService.send("premiumRequired");
