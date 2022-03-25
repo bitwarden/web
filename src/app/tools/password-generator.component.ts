11034de7d1658f2d3654413dadc1298b74a842f9
Kyle Spearrin <kspearrin@users.noreply.github.com>
2022-03-25 11:00:40 -0400

    resolve build errors with latest jslib ref (#1565)

--- src/app/tools/password-generator.component.ts
+++ src/app/tools/password-generator.component.ts
@@ -1,10 +1,13 @@
 import { Component, ViewChild, ViewContainerRef } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 import { PasswordGeneratorComponent as BasePasswordGeneratorComponent } from "jslib-angular/components/password-generator.component";
 import { ModalService } from "jslib-angular/services/modal.service";
 import { I18nService } from "jslib-common/abstractions/i18n.service";
 import { PasswordGenerationService } from "jslib-common/abstractions/passwordGeneration.service";
 import { PlatformUtilsService } from "jslib-common/abstractions/platformUtils.service";
+import { StateService } from "jslib-common/abstractions/state.service";
+import { UsernameGenerationService } from "jslib-common/abstractions/usernameGeneration.service";
 
 import { PasswordGeneratorHistoryComponent } from "./password-generator-history.component";
 
@@ -18,11 +21,22 @@ export class PasswordGeneratorComponent extends BasePasswordGeneratorComponent {
 
   constructor(
     passwordGenerationService: PasswordGenerationService,
+    usernameGenerationService: UsernameGenerationService,
+    stateService: StateService,
     platformUtilsService: PlatformUtilsService,
     i18nService: I18nService,
+    route: ActivatedRoute,
     private modalService: ModalService
   ) {
-    super(passwordGenerationService, platformUtilsService, i18nService, window);
+    super(
+      passwordGenerationService,
+      usernameGenerationService,
+      platformUtilsService,
+      stateService,
+      i18nService,
+      route,
+      window
+    );
   }
 
   async history() {
