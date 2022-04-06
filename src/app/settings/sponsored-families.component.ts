e2ae9ba540eb42ccec068f5afae7179b3a39abef
Matt Gibson <mgibson@bitwarden.com>
2022-04-06 09:59:44 -0400

    [ps-4] f4e Block sponsorship for same email

--- src/app/settings/sponsored-families.component.ts
+++ src/app/settings/sponsored-families.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 
+import { ValidationService } from "jslib-angular/services/validation.service";
 import { ApiService } from "jslib-common/abstractions/api.service";
 import { I18nService } from "jslib-common/abstractions/i18n.service";
 import { OrganizationService } from "jslib-common/abstractions/organization.service";
 import { PlatformUtilsService } from "jslib-common/abstractions/platformUtils.service";
+import { StateService } from "jslib-common/abstractions/state.service";
 import { SyncService } from "jslib-common/abstractions/sync.service";
 import { PlanSponsorshipType } from "jslib-common/enums/planSponsorshipType";
 import { Organization } from "jslib-common/models/domain/organization";
@@ -19,6 +21,7 @@ export class SponsoredFamiliesComponent implements OnInit {
   activeSponsorshipOrgs: Organization[] = [];
   selectedSponsorshipOrgId = "";
   sponsorshipEmail = "";
+  currentUserEmail: string;
 
   // Conditional display properties
   formPromise: Promise<any>;
@@ -28,7 +31,9 @@ export class SponsoredFamiliesComponent implements OnInit {
     private i18nService: I18nService,
     private platformUtilsService: PlatformUtilsService,
     private syncService: SyncService,
-    private organizationService: OrganizationService
+    private organizationService: OrganizationService,
+    private validationService: ValidationService,
+    private stateService: StateService
   ) {}
 
   async ngOnInit() {
@@ -36,6 +41,11 @@ export class SponsoredFamiliesComponent implements OnInit {
   }
 
   async submit() {
+    if (this.currentUserEmail.toLowerCase() === this.sponsorshipEmail.toLowerCase()) {
+      this.validationService.showError(this.i18nService.t("cannotSponsorSelf"));
+      return;
+    }
+
     this.formPromise = this.apiService.postCreateSponsorship(this.selectedSponsorshipOrgId, {
       sponsoredEmail: this.sponsorshipEmail,
       planSponsorshipType: PlanSponsorshipType.FamiliesForEnterprise,
@@ -53,8 +63,10 @@ export class SponsoredFamiliesComponent implements OnInit {
     if (this.loading) {
       return;
     }
-
     this.loading = true;
+
+    this.currentUserEmail = await this.stateService.getEmail();
+
     if (forceReload) {
       await this.syncService.fullSync(true);
     }
