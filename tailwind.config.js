7dfba0fc8214a53f7d096bacc96a9e95c72e11b6
Thomas Rittson <trittson@bitwarden.com>
2022-04-25 11:03:36 +1000

    Update tailwind config to use darkMode: class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-undef, @typescript-eslint/no-var-requires */
 const config = require("./jslib/components/tailwind.config.base");
 
-module.exports = config;
+module.exports = {
+  ...config,
+  darkMode: "class",
+};
