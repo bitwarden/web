5b9382c014566d7d06f4101f14759f66a4dff957
Thomas Rittson <trittson@bitwarden.com>
2022-04-26 14:02:39 +1000

    Get current theme

--- src/app/organizations/settings/subscription-hidden.component.ts
+++ src/app/organizations/settings/subscription-hidden.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 
+import { PlatformUtilsService } from "jslib-common/abstractions/platformUtils.service";
 import { ThemeType } from "jslib-common/enums/themeType";
 
 interface Colors {
@@ -10,8 +11,10 @@ interface Colors {
   selector: "app-subscription-hidden",
   templateUrl: "./subscription-hidden.svg",
 })
-export class SubscriptionHiddenComponent {
-  themes = {
+export class SubscriptionHiddenComponent implements OnInit, OnDestroy {
+  constructor(private platformUtilsService: PlatformUtilsService) {}
+
+  themes: Partial<Record<ThemeType, { shape: string; line: string }>> = {
     [ThemeType.Light]: {
       shape: "#E1E5EE",
       line: "#2F343D",
@@ -22,4 +25,31 @@ export class SubscriptionHiddenComponent {
     },
   };
   colors = this.themes[ThemeType.Light];
+
+  private matchMedia = window.matchMedia("(prefers-color-scheme: dark)");
+  private onThemeChangeCallback = () => this.setColors();
+
+  async ngOnInit() {
+    await this.setColors();
+    try {
+      this.matchMedia.addEventListener("change", this.onThemeChangeCallback);
+    } catch {
+      // < Safari 14
+      this.matchMedia.addListener(this.onThemeChangeCallback);
+    }
+  }
+
+  ngOnDestroy() {
+    try {
+      this.matchMedia.removeEventListener("change", this.onThemeChangeCallback);
+    } catch {
+      // < Safari 14
+      this.matchMedia.removeListener(this.onThemeChangeCallback);
+    }
+  }
+
+  private async setColors() {
+    const theme = await this.platformUtilsService.getEffectiveTheme();
+    this.colors = this.themes[theme];
+  }
 }
