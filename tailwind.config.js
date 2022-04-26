4c2d2ed5801831b40bfc35d34ff15cc1c598b13c
Thomas Rittson <trittson@bitwarden.com>
2022-04-26 10:42:05 +1000

    Revert "Update tailwind config to use darkMode: class"
    This reverts commit 7dfba0fc8214a53f7d096bacc96a9e95c72e11b6.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
 /* eslint-disable no-undef, @typescript-eslint/no-var-requires */
 const config = require("./jslib/components/tailwind.config.base");
 
-module.exports = {
-  ...config,
-  darkMode: "class",
-};
+module.exports = config;
