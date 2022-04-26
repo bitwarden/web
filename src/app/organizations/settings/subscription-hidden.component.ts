05a3b3d81904d50c90bb254fa9e021274088709b
Thomas Rittson <trittson@bitwarden.com>
2022-04-26 13:37:19 +1000

    Add theme colors

--- src/app/organizations/settings/subscription-hidden.component.ts
+++ src/app/organizations/settings/subscription-hidden.component.ts
@@ -1,11 +1,25 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 
+import { ThemeType } from "jslib-common/enums/themeType";
+
+interface Colors {
+  shapeColor: string;
+  lineColor: string;
+}
 @Component({
   selector: "app-subscription-hidden",
   templateUrl: "./subscription-hidden.svg",
 })
 export class SubscriptionHiddenComponent {
-  fillColor = "#E1E5EE"; //dark: 2f343d
-  lineColor = "#2F343D"; //dark: e1e5ee
-  bgColor = "#ffffff"; //dark: 1f242e
+  themes = {
+    [ThemeType.Light]: {
+      shape: "#E1E5EE",
+      line: "#2F343D",
+    },
+    [ThemeType.Dark]: {
+      shape: "#2F343D",
+      line: "#E1E5EE",
+    },
+  };
+  colors = this.themes[ThemeType.Light];
 }
