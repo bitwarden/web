61078e456e8d43e7cc72099e68f763f172b3b669
Thomas Rittson <trittson@bitwarden.com>
2022-04-28 15:01:08 +1000

    Delete unused services

--- src/app/guards/home.guard.ts
+++ src/app/guards/home.guard.ts
@@ -2,18 +2,11 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router } from "@angular/router";
 
 import { AuthService } from "jslib-common/abstractions/auth.service";
-import { StateService } from "jslib-common/abstractions/state.service";
-import { VaultTimeoutService } from "jslib-common/abstractions/vaultTimeout.service";
 import { AuthenticationStatus } from "jslib-common/enums/authenticationStatus";
 
 @Injectable()
 export class HomeGuard implements CanActivate {
-  constructor(
-    private vaultTimeoutService: VaultTimeoutService,
-    private router: Router,
-    private stateService: StateService,
-    private authService: AuthService
-  ) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   async canActivate(route: ActivatedRouteSnapshot) {
     const authStatus = await this.authService.getAuthStatus();
