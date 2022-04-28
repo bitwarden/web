c5e70ee4a88c2d10e16aa73369cf3a35d9951f98
Thomas Rittson <trittson@bitwarden.com>
2022-04-28 15:00:15 +1000

    Update to use new authStatus method

--- src/app/guards/home.guard.ts
+++ src/app/guards/home.guard.ts
@@ -1,22 +1,27 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router } from "@angular/router";
 
+import { AuthService } from "jslib-common/abstractions/auth.service";
 import { StateService } from "jslib-common/abstractions/state.service";
 import { VaultTimeoutService } from "jslib-common/abstractions/vaultTimeout.service";
+import { AuthenticationStatus } from "jslib-common/enums/authenticationStatus";
 
 @Injectable()
 export class HomeGuard implements CanActivate {
   constructor(
     private vaultTimeoutService: VaultTimeoutService,
     private router: Router,
-    private stateService: StateService
+    private stateService: StateService,
+    private authService: AuthService
   ) {}
 
   async canActivate(route: ActivatedRouteSnapshot) {
-    if (!(await this.stateService.getIsAuthenticated())) {
+    const authStatus = await this.authService.getAuthStatus();
+
+    if (authStatus === AuthenticationStatus.LoggedOut) {
       return this.router.createUrlTree(["/login"], { queryParams: route.queryParams });
     }
-    if (await this.vaultTimeoutService.isLocked()) {
+    if (authStatus === AuthenticationStatus.Locked) {
       return this.router.createUrlTree(["/lock"], { queryParams: route.queryParams });
     }
     return this.router.createUrlTree(["/vault"], { queryParams: route.queryParams });
