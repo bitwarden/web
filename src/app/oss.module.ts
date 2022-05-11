fda00fce4bebc7e2db20a8606beccda36ae11791
Thomas Rittson <trittson@bitwarden.com>
2022-05-11 13:07:56 +1000

    Do not import and re-export SharedModule in OssModule

--- src/app/oss.module.ts
+++ src/app/oss.module.ts
@@ -2,25 +2,12 @@ import { NgModule } from "@angular/core";
 
 import { LooseComponentsModule } from "./modules/loose-components.module";
 import { PipesModule } from "./modules/pipes/pipes.module";
-import { SharedModule } from "./modules/shared.module";
 import { VaultFilterModule } from "./modules/vault-filter/vault-filter.module";
 import { OrganizationBadgeModule } from "./modules/vault/modules/organization-badge/organization-badge.module";
 
 @NgModule({
-  imports: [
-    SharedModule,
-    LooseComponentsModule,
-    VaultFilterModule,
-    OrganizationBadgeModule,
-    PipesModule,
-  ],
-  exports: [
-    LooseComponentsModule,
-    VaultFilterModule,
-    OrganizationBadgeModule,
-    PipesModule,
-    SharedModule,
-  ],
+  imports: [LooseComponentsModule, VaultFilterModule, OrganizationBadgeModule, PipesModule],
+  exports: [LooseComponentsModule, VaultFilterModule, OrganizationBadgeModule, PipesModule],
   bootstrap: [],
 })
 export class OssModule {}
