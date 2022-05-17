581ce52b0aa604cfe64a3476a513fdb05ba74f89
Matt Gibson <mgibson@bitwarden.com>
2022-05-17 17:57:59 -0400

    Add verification to org user module

--- src/app/modules/organizations/users/organization-user.module.ts
+++ src/app/modules/organizations/users/organization-user.module.ts
@@ -1,12 +1,13 @@
 import { ScrollingModule } from "@angular/cdk/scrolling";
 import { NgModule } from "@angular/core";
 
+import { LooseComponentsModule } from "../../loose-components.module";
 import { SharedModule } from "../../shared.module";
 
 import { EnrollMasterPasswordReset } from "./enroll-master-password-reset.component";
 
 @NgModule({
-  imports: [SharedModule, ScrollingModule],
+  imports: [SharedModule, ScrollingModule, LooseComponentsModule],
   declarations: [EnrollMasterPasswordReset],
   exports: [EnrollMasterPasswordReset],
 })
