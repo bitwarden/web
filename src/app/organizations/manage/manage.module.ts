44483c2fa48fc3bf57c7dbae16593de93b87bc9c
Thomas Rittson <trittson@bitwarden.com>
2022-06-02 14:24:01 +1000

    Create ManageModule

---
+++ src/app/organizations/manage/manage.module.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+import { SharedModule } from "../../modules/shared.module";
+
+import { BulkConfirmComponent } from "./bulk/bulk-confirm.component";
+import { BulkRemoveComponent } from "./bulk/bulk-remove.component";
+import { BulkStatusComponent } from "./bulk/bulk-status.component";
+import { CollectionAddEditComponent } from "./collection-add-edit.component";
+import { CollectionsComponent as ManageCollectionsComponent } from "./collections.component";
+import { EntityEventsComponent } from "./entity-events.component";
+import { EventsComponent } from "./events.component";
+import { GroupAddEditComponent } from "./group-add-edit.component";
+import { GroupsComponent } from "./groups.component";
+import { ManageComponent } from "./manage.component";
+import { PeopleComponent } from "./people.component";
+import { PolicyEditComponent } from "./policy-edit.component";
+import { ResetPasswordComponent } from "./reset-password.component";
+import { UserAddEditComponent } from "./user-add-edit.component";
+import { UserConfirmComponent } from "./user-confirm.component";
+import { UserGroupsComponent } from "./user-groups.component";
+
+@NgModule({
+  imports: [CommonModule, SharedModule],
+  declarations: [
+    BulkConfirmComponent,
+    BulkRemoveComponent,
+    BulkStatusComponent,
+    CollectionAddEditComponent,
+    EntityEventsComponent,
+    EventsComponent,
+    GroupAddEditComponent,
+    GroupsComponent,
+    ManageCollectionsComponent,
+    ManageComponent,
+    PeopleComponent,
+    PolicyEditComponent,
+    ResetPasswordComponent,
+    UserAddEditComponent,
+    UserConfirmComponent,
+    UserGroupsComponent,
+  ],
+})
+export class ManageModule {}
--- src/app/organizations/organizations.module.ts
+++ src/app/organizations/organizations.module.ts
@@ -6,22 +6,7 @@ import { SharedModule } from "../modules/shared.module";
 
 import { OrganizationLayoutComponent } from "./layouts/organization-layout.component";
 import { OrganizationSwitcherComponent } from "./layouts/organization-switcher.component";
-import { BulkConfirmComponent } from "./manage/bulk/bulk-confirm.component";
-import { BulkRemoveComponent } from "./manage/bulk/bulk-remove.component";
-import { BulkStatusComponent } from "./manage/bulk/bulk-status.component";
-import { CollectionAddEditComponent } from "./manage/collection-add-edit.component";
-import { CollectionsComponent as ManageCollectionsComponent } from "./manage/collections.component";
-import { EntityEventsComponent } from "./manage/entity-events.component";
-import { EventsComponent } from "./manage/events.component";
-import { GroupAddEditComponent } from "./manage/group-add-edit.component";
-import { GroupsComponent } from "./manage/groups.component";
-import { ManageComponent } from "./manage/manage.component";
-import { PeopleComponent } from "./manage/people.component";
-import { PolicyEditComponent } from "./manage/policy-edit.component";
-import { ResetPasswordComponent } from "./manage/reset-password.component";
-import { UserAddEditComponent } from "./manage/user-add-edit.component";
-import { UserConfirmComponent } from "./manage/user-confirm.component";
-import { UserGroupsComponent } from "./manage/user-groups.component";
+import { ManageModule } from "./manage/manage.module";
 import { OrganizationsRoutingModule } from "./organizations-routing.module";
 import { PoliciesModule } from "./policies/policies.module";
 import { SettingsModule } from "./settings/settings.module";
@@ -44,35 +29,20 @@ import { WeakPasswordsReportComponent } from "./tools/weak-passwords-report.comp
     LayoutsModule,
     PoliciesModule,
     SettingsModule,
+    ManageModule,
   ],
   declarations: [
     AcceptFamilySponsorshipComponent,
-    BulkConfirmComponent,
-    BulkRemoveComponent,
-    BulkStatusComponent,
-    CollectionAddEditComponent,
-    EntityEventsComponent,
-    EventsComponent,
     ExportComponent,
     ExposedPasswordsReportComponent,
     FamiliesForEnterpriseSetupComponent,
-    GroupAddEditComponent,
-    GroupsComponent,
     ImportComponent,
     InactiveTwoFactorReportComponent,
-    ManageCollectionsComponent,
-    ManageComponent,
     OrganizationLayoutComponent,
     OrganizationSwitcherComponent,
-    PeopleComponent,
-    PolicyEditComponent,
-    ResetPasswordComponent,
     ReusedPasswordsReportComponent,
     ToolsComponent,
     UnsecuredWebsitesReportComponent,
-    UserAddEditComponent,
-    UserConfirmComponent,
-    UserGroupsComponent,
     WeakPasswordsReportComponent,
   ],
 })
