194d1bb9cf0aae70169ab08b334ca634826e6f55
Thomas Rittson <trittson@bitwarden.com>
2022-06-02 14:27:54 +1000

    Create ToolsModule

--- src/app/organizations/organizations.module.ts
+++ src/app/organizations/organizations.module.ts
@@ -12,14 +12,7 @@ import { PoliciesModule } from "./policies/policies.module";
 import { SettingsModule } from "./settings/settings.module";
 import { AcceptFamilySponsorshipComponent } from "./sponsorships/accept-family-sponsorship.component";
 import { FamiliesForEnterpriseSetupComponent } from "./sponsorships/families-for-enterprise-setup.component";
-import { ExportComponent } from "./tools/export.component";
-import { ExposedPasswordsReportComponent } from "./tools/exposed-passwords-report.component";
-import { ImportComponent } from "./tools/import.component";
-import { InactiveTwoFactorReportComponent } from "./tools/inactive-two-factor-report.component";
-import { ReusedPasswordsReportComponent } from "./tools/reused-passwords-report.component";
-import { ToolsComponent } from "./tools/tools.component";
-import { UnsecuredWebsitesReportComponent } from "./tools/unsecured-websites-report.component";
-import { WeakPasswordsReportComponent } from "./tools/weak-passwords-report.component";
+import { ToolsModule } from "./tools/tools.module";
 
 @NgModule({
   imports: [
@@ -30,20 +23,13 @@ import { WeakPasswordsReportComponent } from "./tools/weak-passwords-report.comp
     PoliciesModule,
     SettingsModule,
     ManageModule,
+    ToolsModule,
   ],
   declarations: [
     AcceptFamilySponsorshipComponent,
-    ExportComponent,
-    ExposedPasswordsReportComponent,
     FamiliesForEnterpriseSetupComponent,
-    ImportComponent,
-    InactiveTwoFactorReportComponent,
     OrganizationLayoutComponent,
     OrganizationSwitcherComponent,
-    ReusedPasswordsReportComponent,
-    ToolsComponent,
-    UnsecuredWebsitesReportComponent,
-    WeakPasswordsReportComponent,
   ],
 })
 export class OrganizationsModule {}
---
+++ src/app/organizations/tools/tools.module.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+import { SharedModule } from "../../modules/shared.module";
+
+import { ExportComponent } from "./export.component";
+import { ExposedPasswordsReportComponent } from "./exposed-passwords-report.component";
+import { ImportComponent } from "./import.component";
+import { InactiveTwoFactorReportComponent } from "./inactive-two-factor-report.component";
+import { ReusedPasswordsReportComponent } from "./reused-passwords-report.component";
+import { ToolsComponent } from "./tools.component";
+import { UnsecuredWebsitesReportComponent } from "./unsecured-websites-report.component";
+import { WeakPasswordsReportComponent } from "./weak-passwords-report.component";
+
+@NgModule({
+  imports: [CommonModule, SharedModule],
+  declarations: [
+    ExportComponent,
+    ExposedPasswordsReportComponent,
+    ImportComponent,
+    InactiveTwoFactorReportComponent,
+    ReusedPasswordsReportComponent,
+    ToolsComponent,
+    UnsecuredWebsitesReportComponent,
+    WeakPasswordsReportComponent,
+  ],
+})
+export class ToolsModule {}
