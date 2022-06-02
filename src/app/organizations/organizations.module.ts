69bf3d7fb7fd65491ba69cfeef7de4a795b6459e
Thomas Rittson <trittson@bitwarden.com>
2022-06-02 15:41:01 +1000

    Remove eager loading of module

--- src/app/organizations/organizations.module.ts
+++ src/app/organizations/organizations.module.ts
@@ -7,12 +7,11 @@ import { SharedModule } from "../modules/shared.module";
 import { OrganizationLayoutComponent } from "./layouts/organization-layout.component";
 import { OrganizationSwitcherComponent } from "./layouts/organization-switcher.component";
 import { OrganizationsRoutingModule } from "./organizations-routing.module";
-import { SettingsModule } from "./settings/settings.module";
 import { AcceptFamilySponsorshipComponent } from "./sponsorships/accept-family-sponsorship.component";
 import { FamiliesForEnterpriseSetupComponent } from "./sponsorships/families-for-enterprise-setup.component";
 
 @NgModule({
-  imports: [CommonModule, OrganizationsRoutingModule, SharedModule, LayoutsModule, SettingsModule],
+  imports: [CommonModule, OrganizationsRoutingModule, SharedModule, LayoutsModule],
   declarations: [
     AcceptFamilySponsorshipComponent,
     FamiliesForEnterpriseSetupComponent,
