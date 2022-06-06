c233afcdf5e06a2e7b5a7380370f86f6fabdbcfe
Addison Beck <addisonbeck1@gmail.com>
2022-06-06 13:28:10 -0400

    [fix] Pull org admin vault collections directly from the API (#1700)
    * [fix] Pull org admin vault collections directly from the API
    
    * [refactor] Establish pattern for interfaces in module scoped services
    
    * [fix] Remove access modifiers from constructor params for an extended service
    
    * [fix] Rename skipFilterCollectionRefresh to firstLoaded
    
    * [fix] Simplify hiding unwanted filters in the org vault
    
    * Revert "[refactor] Establish pattern for interfaces in module scoped services"
    
    This reverts commit f1edae5a3e466fd0a43c7741aa060295508e2a08.
    
    * Update src/app/modules/vault-filter/vault-filter.component.ts
    
    Co-authored-by: Thomas Rittson <31796059+eliykat@users.noreply.github.com>
    
    * Update src/app/modules/vault-filter/organization-vault-filter.component.ts
    
    Co-authored-by: Thomas Rittson <31796059+eliykat@users.noreply.github.com>
    
    * [fix] Ran prettier
    
    Co-authored-by: Thomas Rittson <31796059+eliykat@users.noreply.github.com>

--- src/app/modules/vault-filter/vault-filter.component.ts
+++ src/app/modules/vault-filter/vault-filter.component.ts
@@ -1,34 +1,26 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 import { VaultFilterComponent as BaseVaultFilterComponent } from "jslib-angular/modules/vault-filter/vault-filter.component";
-import { VaultFilterService } from "jslib-angular/modules/vault-filter/vault-filter.service";
-import { Organization } from "jslib-common/models/domain/organization";
+
+import { VaultFilterService } from "./vault-filter.service";
 
 @Component({
   selector: "app-vault-filter",
   templateUrl: "vault-filter.component.html",
 })
 export class VaultFilterComponent extends BaseVaultFilterComponent {
-  @Input() showOrgFilter = true;
-  @Input() showFolders = true;
-  @Input() showFavorites = true;
-
   @Output() onSearchTextChanged = new EventEmitter<string>();
 
   searchPlaceholder: string;
   searchText = "";
 
-  organization: Organization;
-
-  constructor(vaultFilterService: VaultFilterService) {
+  constructor(protected vaultFilterService: VaultFilterService) {
+    // This empty constructor is required to provide the web vaultFilterService subclass to super()
+    // TODO: refactor this to use proper dependency injection
     super(vaultFilterService);
   }
 
   searchTextChanged() {
     this.onSearchTextChanged.emit(this.searchText);
-  }
-
-  async initCollections() {
-    return await this.vaultFilterService.buildCollections(this.organization?.id);
   }
 }
--- src/app/modules/vault-filter/vault-filter.service.ts
+++ src/app/modules/vault-filter/vault-filter.service.ts
@@ -1,3 +1,54 @@
-import { VaultFilterService as BaseVaultFilterService } from "jslib-angular/modules/vault-filter/vault-filter.service";
+import { Injectable } from "@angular/core";
 
-export class VaultFilterService extends BaseVaultFilterService {}
+import { DynamicTreeNode } from "jslib-angular/modules/vault-filter/models/dynamic-tree-node.model";
+import { VaultFilterService as BaseVaultFilterService } from "jslib-angular/modules/vault-filter/vault-filter.service";
+import { ApiService } from "jslib-common/abstractions/api.service";
+import { CipherService } from "jslib-common/abstractions/cipher.service";
+import { CollectionService } from "jslib-common/abstractions/collection.service";
+import { FolderService } from "jslib-common/abstractions/folder.service";
+import { OrganizationService } from "jslib-common/abstractions/organization.service";
+import { PolicyService } from "jslib-common/abstractions/policy.service";
+import { StateService } from "jslib-common/abstractions/state.service";
+import { CollectionData } from "jslib-common/models/data/collectionData";
+import { Collection } from "jslib-common/models/domain/collection";
+import { CollectionDetailsResponse } from "jslib-common/models/response/collectionResponse";
+import { CollectionView } from "jslib-common/models/view/collectionView";
+
+@Injectable()
+export class VaultFilterService extends BaseVaultFilterService {
+  constructor(
+    stateService: StateService,
+    organizationService: OrganizationService,
+    folderService: FolderService,
+    cipherService: CipherService,
+    collectionService: CollectionService,
+    policyService: PolicyService,
+    protected apiService: ApiService
+  ) {
+    super(
+      stateService,
+      organizationService,
+      folderService,
+      cipherService,
+      collectionService,
+      policyService
+    );
+  }
+
+  async buildAdminCollections(organizationId: string) {
+    let result: CollectionView[] = [];
+    const collectionResponse = await this.apiService.getCollections(organizationId);
+    if (collectionResponse?.data != null && collectionResponse.data.length) {
+      const collectionDomains = collectionResponse.data.map(
+        (r: CollectionDetailsResponse) => new Collection(new CollectionData(r))
+      );
+      result = await this.collectionService.decryptMany(collectionDomains);
+    }
+
+    const nestedCollections = await this.collectionService.getAllNested(result);
+    return new DynamicTreeNode<CollectionView>({
+      fullList: result,
+      nestedList: nestedCollections,
+    });
+  }
+}
